358dd19f8b5db22da5026ff5731a14a58d969c56
horus018 <rubira820@gmail.com>
2024-04-21 20:41:19 -0300

    - Adicao da funcao para ler inputs das teclas na tela - Adicao da variavel currentplayerid para controlar o usuario atual

--- 1st-release/client.js
+++ 1st-release/client.js
@@ -1,5 +1,6 @@
 const screen = $('#screen').get(0)
 const context = screen.getContext('2d')
+const currentPlayerId = 'player1'
 
 const game = {
     players: {
@@ -11,19 +12,45 @@ const game = {
     }
 }
 
+document.addEventListener('keydown', handleKeydown)
+
+function handleKeydown(event) {
+    const player = game.players[currentPlayerId]
+
+    if(event.key === 'w'){
+        player.y = player.y - 1
+        return
+    }
+
+    if(event.key === 'a'){
+        player.x = player.x - 1
+        return
+    }
+
+    if(event.key === 's'){
+        player.y = player.y + 1
+        return
+    }
+
+    if(event.key === 'd'){
+        player.x = player.x + 1
+        return
+    }
+}
+
 renderScreen()
 
 function renderScreen() {
     context.fillStyle = 'white'
     context.clearRect(0, 0, 10, 10)
 
-    for (playerId in game.players) {
+    for (const playerId in game.players) {
         const player = game.players[playerId]
         context.fillStyle = 'black'
         context.fillRect(player.x, player.y, 1, 1)
     }
 
-    for (fruitId in game.fruits) {
+    for (const fruitId in game.fruits) {
         const fruit = game.fruits[fruitId]
         context.fillStyle = 'green'
         context.fillRect(fruit.x, fruit.y, 1, 1)
